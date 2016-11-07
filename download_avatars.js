bbef5770b9fb4abba4315ecc0c84819089c149b7
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-11-07 22:52:29 +0000

    Finished the getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,28 @@
 var request = require('request');
+var user = require('./user')
 
-console.log("Welcome to the Github Avatar Downloader!")
+console.log('Welcome to the Github Avatar Downloader!');
 
+function getRepoContributors (repoOwner, repoName, cb){
+  var path = 'repos/jquery/jquery/contributors'
+  request.get({
+            url: 'http://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+            auth: {
+              user: user.name,
+              pass: user.token
+            },
+            headers: {
+              'User-Agent': 'Firefox'
+            }
+          }, function (error, response, body){
+    var data = JSON.parse(body);
+    data.forEach(function(avatars){
+      console.log(avatars)
+    })
+  })
+};
+
+getRepoContributors("nodejs", "node", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
