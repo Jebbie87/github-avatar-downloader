6d9a243b81e13a9cafc7a52e08c6610e48f8062c
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-11-08 01:23:58 +0000

    Connected the two functions together

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,9 @@
 'user strict';
 const request = require('request');
-const user = require('./user')
-const fs = require('fs')
-const GITHUB_USER = user.name
-const GITHUB_TOKEN = user.token
+const user = require('./user');
+const fs = require('fs');
+const GITHUB_USER = user.name;
+const GITHUB_TOKEN = user.token;
 
 console.log('Welcome to the Github Avatar Downloader!');
 
@@ -11,35 +11,26 @@ function getRepoContributors (repoOwner, repoName, cb){
   const requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   request.get({
             url: requestURL,
-            auth: {
-              user: user.name,
-              pass: user.token
-            },
             headers: {
               'User-Agent': 'Firefox'
             }
           }, function (error, response, body){
      const data = JSON.parse(body);
-
-    // Will print the parsed data as a JSON object to terminal
-     data.forEach(function(avatars){
-       console.log(avatars['avatar_url'])
-     })
-  })
+     cb(data);
+  });
 };
 
 function downloadImageByURL (url, urlPath){
-    console.log("Downloading avatar image...")
-    request.get(url)
-           .on('end', function(){
-             console.log("Finished downloading avatar image!")
-           })
-           .pipe(fs.createWriteStream('./avatar-image.jpg'))
+    let filePath = "./avatars/" + urlPath + ".jpg";
+    request.get(url + urlPath)
+           .pipe(fs.createWriteStream(filePath));
 }
 
-downloadImageByURL('https://avatars2.githubusercontent.com/', 'u/2741?v=3&s=466", "avatars/kvirani.jpg')
-
-// getRepoContributors("nodejs", "node", function(err, result) {
-//   console.log("Errors:", err);
-//   console.log("Result:", result);
-// });
+downloadImageByURL("https://avatars.githubusercontent.com/u/1935767?v=3", 'yorkie')
+getRepoContributors("nodejs", "node", function(url){
+  console.log("Downloading avatar images!");
+  url.forEach(function(address){
+    downloadImageByURL(address['avatar_url'], address['login'])
+  })
+  console.log("All avatar images downloaded!");
+});
