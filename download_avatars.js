49b8472b33410e83274dc7c9fac18e252d33878b
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-11-08 01:53:19 +0000

    Made it so that the command line arguments are required

--- download_avatars.js
+++ download_avatars.js
@@ -4,30 +4,29 @@ const user = require('./user');
 const fs = require('fs');
 const GITHUB_USER = user.name;
 const GITHUB_TOKEN = user.token;
-const person = process.argv.slice(2)
+const person = process.argv.slice(2);
 
-console.log('Welcome to the Github Avatar Downloader!');
-
-function getRepoContributors (repoOwner, repoName, cb){
-  const requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  request.get({
-            url: requestURL,
-            headers: {
-              'User-Agent': 'Firefox'
-            }
-          }, function (error, response, body){
-     const data = JSON.parse(body);
-     cb(data);
-  });
-};
+if (person.length === 2){
+  console.log('Welcome to the Github Avatar Downloader!');
+  function getRepoContributors (repoOwner, repoName, cb){
+    const requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+    request.get({
+              url: requestURL,
+              headers: {
+                'User-Agent': 'Firefox'
+              }
+            }, function (error, response, body){
+    const data = JSON.parse(body);
+    cb(data);
+    });
+  };
 
 function downloadImageByURL (url, urlPath){
-    let filePath = "./avatars/" + urlPath + ".jpg";
-    request.get(url + urlPath)
-           .pipe(fs.createWriteStream(filePath));
+  let filePath = "./avatars/" + urlPath + ".jpg";
+  request.get(url + urlPath)
+         .pipe(fs.createWriteStream(filePath));
 }
 
-downloadImageByURL("https://avatars.githubusercontent.com/u/1935767?v=3", 'yorkie')
 getRepoContributors(person[0], person[1], function(url){
   console.log("Downloading avatar images!");
   url.forEach(function(address){
@@ -35,3 +34,8 @@ getRepoContributors(person[0], person[1], function(url){
   })
   console.log("All avatar images downloaded!");
 });
+} else {
+  console.log("Please put in a valid support line argument!");
+  console.log("The right form is 'node <file name> <repo owner> <repo name>")
+  console.log("An example command is --> node download_avatars.js nodejs node")
+};
